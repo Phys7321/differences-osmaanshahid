7e0747c80b81ca27b994b07b7393872abce7af6d
osmaanshahid <shahid.o@husky.neu.edu>
2018-09-15 20:07:16 -0400

    Escape

--- Python/myder.py
+++ Python/myder.py
@@ -3,23 +3,33 @@
 Created on Sun Sep  9 16:50:22 2018
 
 @author: Tom K
+edited by: Osmaan S
 """
+
+# Define the function f(x)
+def f(x):
+    return x**2
+
+# Define forward difference method for derivative, f'(x)
+# over the interval [a,b] for N points
+# N is
+# h is
 
 def forwardiff(f,a,b,N):
     h = (b-a)/N
-    g=[]
+    y_prime=[]
     for k in range(1,N):
         slop = (f(a+k*h)-f(a+(k-1)*h))/h
-        g.append(slop)
-    return array(g)
+        y_prime.append(slop)
+    return y_prime
     
 def backwardiff(f,a,b,N):
     h = (b-a)/N
-    g=[]
+    y_prime=[]
     for k in range(0,N-1):
         slop = (f(a+(k+1)*h)-f(a+k*h))/h
-        g.append(slop)
-    return array(g)
+        y_prime.append(slop)
+    return y_prime
 
 def centraldiff(f,a,b,N):
     h = (b-a)/N
