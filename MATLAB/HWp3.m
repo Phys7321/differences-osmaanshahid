f0e17958fcbbfdaeab27e36e5cc4e3829b3c0837
osmaanshahid <shahid.o@husky.neu.edu>
2018-09-17 15:21:31 -0400

    Updated HWp3.m

--- MATLAB/HWp3.m
+++ MATLAB/HWp3.m
@@ -1,28 +1,33 @@
-function [dy,xc] = HWp3(varargin)
-switch nargin
-    case 2
-        F = varargin{1};
-        x = varargin{2};
-        method = 'cd' ; %default value
-        n = 5 ; %default value
-    case 3
-        F = varargin{1};
-        x = varargin{2};
-        method = varargin{3};
-        n = 5 ; %default value
-    case 4
-        F = varargin{1};
-        x = varargin{2};
-        method = varargin{3};
-        n = varargin{4};
-end
-
+function [dy,xc] = HWp3()
 load('derdata.mat')
 
-[dy,xc] = Der(@func,X);
-figure(3);
-plot(X,Y);
+figure(1)
+plot(X,Y)
+grid
+
+if ~isnumeric(X)
+    error('Second input must be numeric vector')
+end
+n = length(X);
+if ~isa(Y,'function_handle') % If F is NOT a fxn handle, continue
+    if isnumeric(Y)          % If F is a numeric vector, continue
+      xc = X(1:n-1); % method for data depends on how you assign dy to xc
+      dy = diff(Y)./diff(X); 
+      method = 'data';
+    else
+      error('First input must be a function handle or a numeric array')
+    end
+end
+
+figure(2);
+plot(xc,dy);
 grid;
 
+dyy = diff(dy)./diff(xc); %Second derivative
+xcc = X(1:n-2);
+
+figure(3);
+plot(xcc,dyy);
+grid;
 
 end
